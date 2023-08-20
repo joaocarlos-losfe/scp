27bc35adde51bf2e1b54c873425f6916d76f5013
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2023-08-20 15:16:27 -0300

    up

---
+++ comando.sh
@@ -0,0 +1,24 @@
+#variaveis
+host_destino="root"@192.168.1.4
+pass="9009"
+container_origem="df2"
+container_destino="a10"
+checkpoint_dir="~/docker_checkpoints"
+
+#copia dos arquivos para migração
+echo "Inicio da copia:"
+echo ""
+
+sshpass -p "$pass" scp -r /var/lib/docker/containers/"$container_origem"/checkpoints/ "$host_destino":"$checkpoint_dir"
+sshpass -p "$pass" ssh -t "$host_destino" "cp -r "$checkpoint_dir/checkpoints/*"
+/var/lib/docker/containers/"$container_destino"/checkpoints/"
+
+echo "copiado"
+echo ""
+
+echo "restaurando"
+echo ""
+sshpass -p "$pass" ssh -t "$host_destino" "docker start --checkpoint check1 $container_destino" 
+
+echo "fim da migração"
+echo ""
