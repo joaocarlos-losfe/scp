a5fb4a456916e892dc6aa6d590d9b2de18b1f4c6
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2023-08-20 10:32:04 -0300

    crio sd

---
+++ stopandcopy.sh
@@ -0,0 +1,72 @@
+
+# Set variables
+CONTAINER_ORI=pasid
+CONTAINER_DESTI=pasid
+CHECKPOINT_DIR=/tmp/
+DEST_HOST=host@192.168.0.111
+CHECKPOINT_NAME=clone
+NOME_CONT_ORIGEM=d15dbab98206a1b01e8b5deb090234c60776aee1715fcdf54f67277f8e518470
+NOME_CONT_DESTI=9a15f70d15103e793877e31d64e53796d89c12f304f87444df0a4c3f8d1ee974
+
+
+inicio_check=$(date "+%M:%S.%3N")
+echo "inicio do checkpoint: $inicio_check"
+echo ""
+echo ""
+echo ""
+
+docker checkpoint create --leave-running pasid $CHECKPOINT_NAME
+fim_check=$(date "+%M:%S.%3N")
+
+echo ""
+echo ""
+echo ""
+
+echo "fim do checkpoint: $fim_check"
+
+echo ""
+echo ""
+echo ""
+
+
+inicio_cop=$(date "+%M:%S.%3N")
+echo "inicio da copia: $inicio_cop"
+
+echo ""
+echo ""
+echo ""
+
+scp -r /var/lib/docker/containers/$NOME_CONT_ORIGEM/checkpoints/ $DEST_HOST:$CHECKPOINT_DIR
+ssh -t $DEST_HOST "cp -r /tmp/checkpoints/ /var/lib/docker/containers/$NOME_CONT_DESTI/"
+
+echo ""
+echo ""
+echo ""
+
+
+fim_cop=$(date "+%M:%S.%3N")
+echo "fim da copia: $fim_cop"
+
+echo ""
+echo ""
+echo ""
+
+
+
+inicio_start=$(date "+%M:%S.%3N")
+echo "inicio do start: $inicio_start"
+echo ""
+echo ""
+echo ""
+
+
+ssh -t $DEST_HOST "docker start --checkpoint $CHECKPOINT_NAME pasid"
+
+fim_start=$(date "+%M:%S.%3N")
+echo "fim do start: $fim_start"
+
+echo ""
+echo ""
+echo ""
+
+
